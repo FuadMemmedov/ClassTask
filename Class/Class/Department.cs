4f328a4c6426ef4b5c6d1d20b2a708624e6c46fe
FuadMemmedov <tu7m43u29@code.edu.az>
2024-03-23 02:54:41 +0400

    task

---
+++ Class/Class/Department.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Department
+    {
+        Employee[] Employees = new Employee[] {};
+
+        public void AddEmployee( Employee employee)
+        {
+            Array.Resize(ref Employees, Employees.Length +1);
+            Employees[Employees.Length - 1] = employee;
+        }
+        public Employee[] GetAllEmployees()
+        {
+            return Employees;
+        }
+
+        public void ShowEmployeeInfo() 
+        {
+            for ( int i = 0; i < Employees.Length; i++)
+            {
+                Console.WriteLine($" Name: {Employees[i].Name} Surname: {Employees[i].Surname} Age: {Employees[i].Age} " +
+                    $" Departmentname: {Employees[i].DepartmentName} Salary: {Employees[i].Salary}");
+            }
+            
+        }
+        public void GetAllEmployeesBySalary(int minSalary,int maxSalary)
+        {
+            for ( int i = 0;i < Employees.Length;i++)
+            {
+                if (minSalary < Employees[i].Salary  && maxSalary > Employees[i].Salary) 
+                {
+                    Console.WriteLine($" Name: {Employees[i].Name} Surname: {Employees[i].Surname} Age: {Employees[i].Age} " +
+                    $" Departmentname: {Employees[i].DepartmentName} Salary: {Employees[i].Salary}");
+                }
+            }
+            
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel.Design;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department department = new Department();
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Isci elave etmek ucun: 1\n" +
+                    "Butun iscilere baxmaq ucun: 2\n" +
+                    "Maas aralığına göre işçileri axtarış et: 3\n" +
+                    "Programi bitir: 0");
+                 answer = Console.ReadLine();
+                if(answer == "1")
+                {
+                    Console.Write("Iscinin adini daxil edin: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Iscinin soyadini daxil edin: ");
+                    string surname = Console.ReadLine();
+                    string ageStr = "";
+                    byte age;
+                    do
+                    {
+                       Console.Write("Iscinin yasini daxil edin: ");
+                       ageStr = Console.ReadLine();
+
+                    } while (!byte.TryParse(ageStr, out age));
+                      Console.Write("Iscinin aid oldugu departman adini yazin: ");
+                      string departmentName = Console.ReadLine();
+                      string salaryStr = "";
+                      int salary;
+                    do
+                    {
+                        Console.Write("Iscinin maasini daxil edin: ");
+                        salaryStr = Console.ReadLine();
+                    } while (!int.TryParse(salaryStr, out salary));
+
+                        Employee employee = new Employee(name, surname, age, departmentName,salary);
+
+
+                         department.AddEmployee(employee);
+
+                }
+                else if (answer == "2")
+                {
+                    department.ShowEmployeeInfo();
+                }
+                else if (answer == "3")
+                {
+                    string minStr = "";
+                    int minSalary;
+
+                    do
+                    {
+                        Console.Write("Min maas daxil edin: ");
+                        minStr = Console.ReadLine();
+
+                    } while (!int.TryParse(minStr, out minSalary));
+                    string maxStr = "";
+                    int maxSalary;
+                    do
+                    {
+                        Console.Write("Max maas daxil edin: ");
+                        maxStr = Console.ReadLine();
+
+                    } while (!int.TryParse(maxStr, out maxSalary));
+                    department.GetAllEmployeesBySalary(minSalary, maxSalary);
+                }
+
+            } while (answer != "0");
+            
+
+        }
+    }
+}
